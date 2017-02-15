9414ab54fc4c81a09f49a30b15d1d55916fb4653
damaratix <damaratix@gmail.com>
2017-02-15 09:32:23 -0500

    added explicit legend for message-type

--- dhcp.py
+++ dhcp.py
@@ -31,24 +31,30 @@ def show_packet_layers(pkt):
 def show_dhcp_options(pkt):
 
     out ={}
-    print '==========================================='
-    print "Eth src mac: %s" % pkt.src
 
+    # store the ip ACK for message-type 5
+    bootp = pkt.getlayer(BOOTP)
     dhcp = pkt.getlayer(DHCP)
     
     # 
     binary_repr = ('client_id','param_req_list','81')
     skip_repr = ('pad','end')
     server_services = ('server_id','name_server','router')
+    msg_type_legend = ('','Discover','Offer','Request','Decline','ACK','NACK','Release','Info')
     
     for opt in dhcp.options:
         if str(opt[0]) in binary_repr:
             print "%s: %s" % (opt[0], binascii.hexlify(opt[1]) )
         elif opt in skip_repr:
-            pass    
+            pass
+        elif opt[0] == 'message-type':
+            if opt[1] == 5:
+                print "%s: %s (%s for %s)" % (opt[0], opt[1], msg_type_legend[opt[1]], bootp.yiaddr)
+            else:
+                print "%s: %s (%s)" % (opt[0], opt[1], msg_type_legend[opt[1]])
         else:
             print "%s: %s" % (opt[0],opt[1])
-        
+
         if opt[0] in server_services:
             out[opt[0]] = opt[1]
 
@@ -60,10 +66,17 @@ if __name__ == "__main__":
     dhcpservers=set()
     nameservers=set()
 
-    packets = rdpcap('dhcp-traffic.pcap.')
+    packets = rdpcap('dhcp-traffic.pcap')
     for pkt in packets:
-        #show_packet(pkt)
-        #show_packet_layers(pkt)
+        # show_packet(pkt)
+        # show_packet_layers(pkt)
+
+        print '==========================================='
+        print "Eth %s > %s" % (pkt.src, pkt.dst)
+
+        ip = pkt.getlayer(IP)
+        print "IP %s > %s" % (ip.src, ip.dst)
+
         buf = show_dhcp_options(pkt)
         try:
             if buf['router']: routers.add(buf['router'])
@@ -75,7 +88,7 @@ if __name__ == "__main__":
             if buf['server_id']: dhcpservers.add(buf['server_id'])
         except: pass
 
-    print 
+    print
     print "Servers Found:"
     print "routers: %s" % (routers)
     print "nameservers: %s " % (nameservers)
